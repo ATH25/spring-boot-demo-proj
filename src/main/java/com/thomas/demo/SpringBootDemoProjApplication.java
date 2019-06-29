5ef7d022ff47e5a141a45ee146cff7e07c41cd2e
ATom <aju_thomas@yahoo.com>
2019-06-28 22:39:44 -0400

    first commit

---
+++ src/main/java/com/thomas/demo/SpringBootDemoProjApplication.java
@@ -0,0 +1,14 @@
+package com.thomas.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoProjApplication.class, args);
+	}
+
+}
+
